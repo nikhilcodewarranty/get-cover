89c50e2d91509f855342c7e419830a1cae85fef1
webdeveloper510 <developerweb6@gmail.com>
2023-11-23 02:22:20 +0530

    changes

--- UserService/userServer.js
+++ UserService/userServer.js
@@ -4,7 +4,7 @@ const mongoose = require("mongodb").MongoClient;
 const bodyParser = require("body-parser");
 require("dotenv").config();
 const app = express();
-const port = process.env.BOOKS_API_PORT || 8080;
+const port = process.env.USER_API_ENDPOINT || 8080;
 const dbConfig = require("./config/database");
 const { databaseConnect } = require("./db");
 
