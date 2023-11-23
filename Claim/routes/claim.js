5719bcdff8fe7eb05e427d5e523aa6cad1247726
webdeveloper510 <developerweb6@gmail.com>
2023-11-23 21:37:51 +0530

    changes by amit

--- Claim/routes/claim.js
+++ Claim/routes/claim.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const claimController = require("../controller/claim");
 
 router.get("/claim", claimController.getAllClaims);
+router.post("/claim/create-claim", claimController.createClaim);
 
 module.exports = router;
---
+++ server.js
@@ -0,0 +1,25 @@
+'use strict';
+
+//mongoose file must be loaded before all other files in order to provide
+// models to other modules
+var express = require('express'),
+  router = express.Router(),
+  bodyParser = require('body-parser'),
+  swaggerUi = require('swagger-ui-express'),
+  swaggerDocument = require('./swagger.json');
+;
+
+
+var app = express();
+
+//rest API requirements
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+//app.use('/api/v1', router);
+
+app.listen(3000);
+module.exports = app;
