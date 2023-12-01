4a755e5e35dc870647beabab42969b32154c3144
webdeveloper510 <developerweb6@gmail.com>
2023-12-01 13:13:40 +0530

    deleted env from all

--- User/db.js
+++ User/db.js
@@ -1,16 +1,21 @@
-require("dotenv").config();
+require("dotenv").config()
+
+
 const mongoose = require('mongoose')
-let mongouri = process.env.DB_URL + 'User'
-console.log('url check-',mongouri)
-exports.databaseConnect = async (mongouri) => {
-  console.log(mongouri)
-  try {
-    await mongoose.connect(mongouri);
-    console.log("database connection connected");
-  } catch (error) {
-    console.log(
-      "🚀 ~ file: mongoDb.js:8 ~ exports.databaseConnect= ~ error:",
-      error
-    );
-  }
-};
+
+const dbUrl = process.env.DB_URL + 'User'
+
+
+const connection = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}
+
+mongoose
+    .connect(dbUrl, connection)
+    .then((res) => {
+        console.info('Connected to db')
+    })
+    .catch((e) => {
+        console.log('Unable to connect to the db', e)
+    })
