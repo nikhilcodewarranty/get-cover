ab23c6a4bef412dc3d274b91d12861a24898c6d8
webdeveloper510 <webdeveloper510>
2023-12-13 18:30:45 +0530

    email template ak

--- config/emailConstant.js
+++ config/emailConstant.js
@@ -3,8 +3,13 @@ module.exports = {
         return {
           to: toEmail,
           from: 'anil@codenomad.net',
-          subject: `Sending an email using SendGrid`,
-          text: `Set Password Link:- http://15.207.221.207/newPassword/${ID}/${resetCode}`,
+          // subject: `Sending an email using SendGrid`,
+          // text: `Set Password Link:- http://15.207.221.207/newPassword/${ID}/${resetCode}`,
+          templateId: "d-db59015a8a074d099cb138bbafc1c54d",
+          dynamic_template_data: {
+            ID:ID,
+            resetCode:resetCode
+          }
         };
       }
 }
