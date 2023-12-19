240264823d4afc9aaa7895eb71fb835fdaae354a
webdeveloper510 <webdeveloper510>
2023-12-19 17:26:24 +0530

    file upload

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/uploads', express.static('./uploads/'))
 
 //common routing for server
 app.use("/api-v1/user", userRoutes);
