08e064bdcb646b5e058f3948399689ce9059d5c6
webdeveloper510 <developerweb6@gmail.com>
2023-12-22 22:28:29 +0530

    get dealer book

--- Dealer/validators/create_dealer_price_book.js
+++ Dealer/validators/create_dealer_price_book.js
@@ -4,7 +4,8 @@ const create_dealer_price_book_validation = Joi.object({
     dealerId:Joi.string().trim().hex().length(24),
     priceBook:Joi.string().trim().hex().length(24),
     retailPrice:Joi.number().required(),
-    status:Joi.boolean().required()
+    status:Joi.boolean().required(),
+    brokerFee:Joi.number().required()
 })
 
 
