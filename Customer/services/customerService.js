e8c8f9a157ac2bda5947edd30a990319d3581379
webdeveloper510 <webdeveloper510>
2024-01-01 13:29:23 +0530

    name change customer

--- Customer/services/customerService.js
+++ Customer/services/customerService.js
@@ -22,7 +22,7 @@ module.exports = class customerService {
   static async getCustomerByName(accountName) {
     try {
       const singleCustomerResponse = await customer.findOne({
-        userName: accountName,
+        username: accountName,
       });
       return singleCustomerResponse;
     } catch (error) {
