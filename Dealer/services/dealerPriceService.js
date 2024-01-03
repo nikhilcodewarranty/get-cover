d31bdb09218fe92d01306a2dab8d93d32ded10d8
webdeveloper510 <developerweb6@gmail.com>
2024-01-03 16:44:11 +0530

    get dealer book

--- Dealer/services/dealerPriceService.js
+++ Dealer/services/dealerPriceService.js
@@ -143,19 +143,18 @@ module.exports = class dealerPriceService {
             localField: "priceBook",
             foreignField: "_id",
             as: "priceBooks",
-          },
-        },
-        {
-          $unwind: '$priceBooks',
-        },
-        {
-          $lookup: {
-            from: "pricecategories",
-            localField: "priceBooks.category",
-            foreignField: "_id",
-            as: "priceBooks.category",
-          },
-        },   
+            pipeline:[
+              {
+                $lookup: {
+                  from: "pricecategories",
+                  localField: "category",
+                  foreignField: "_id",
+                  as: "category"
+                }
+              }
+            ]
+          }
+        }, 
         {
           $lookup: {
             from: "dealers",
--- User/controller/usersController.js
+++ User/controller/usersController.js
@@ -1243,7 +1243,7 @@ exports.createDealer = async (req, res) => {
               }
 
               // Send email with the CSV file link
-              const mailing = await sgMail.send(emailConstant.sendCsvFile('nikhil@codenomad.net', entriesData));
+              const mailing = await sgMail.send(emailConstant.sendCsvFile('amit@codenomad.net', entriesData));
 
               res.send({
                 code: constant.successCode,
