bec63d910754c4681d8d0e8e3419a9a02538d591
webdeveloper510 <developerweb6@gmail.com>
2024-01-05 12:07:42 +0530

    uploads dealer

--- Dealer/controller/dealerController.js
+++ Dealer/controller/dealerController.js
@@ -1038,6 +1038,8 @@ function uniqByKeepLast(data, key) {
 exports.uploadPriceBook = async (req, res) => {
   try {
     // Check if a file is uploaded
+
+   // console.log("filesPath=========",req.file);return;
     if (req.role != "Super Admin") {
       res.send({
         code: constant.errorCode,
@@ -1076,13 +1078,13 @@ exports.uploadPriceBook = async (req, res) => {
       })
       return;
     }
-    // if (checkDealer[0].status == 'Pending') {
-    //   res.send({
-    //     code: constant.errorCode,
-    //     message: "Dealer has not been approved yet!"
-    //   })
-    //   return;
-    // }
+    if (checkDealer[0].status == 'Pending') {
+      res.send({
+        code: constant.errorCode,
+        message: "Dealer has not been approved yet!"
+      })
+      return;
+    }
     let priceBookName = [];
     let csvStatus = [];
     let newArray1;
@@ -1135,9 +1137,8 @@ exports.uploadPriceBook = async (req, res) => {
       let unique = uniqByKeepLast(results, it => it.priceBook)
 
       priceBookName = unique.map(obj => obj.priceBook);
-      const priceBookName1 = results.map(name => new RegExp(`${name.priceBook}`, 'i'));
+      const priceBookName1 = results.map(name => new RegExp(`^${name.priceBook}$`, 'i'));
       const foundProducts = await priceBookService.findByName(priceBookName1);
-      // return;
       if (foundProducts.length > 0) {
         const count = await dealerPriceService.getDealerPriceCount();
         // Extract the names and ids of found products
@@ -1171,8 +1172,6 @@ exports.uploadPriceBook = async (req, res) => {
         const inactiveNames = inactiveData.map(inactive => inactive.name.toLowerCase());
         // Remove product from csv based on inactive name
         priceBookName = priceBookName.filter(name => !inactiveNames.includes(name.toLowerCase()));
-
-
 
         const missingProductNames = priceBookName.filter(name => !foundProductData.some(product => product.name.toLowerCase() === name.toLowerCase()));
         if (missingProductNames.length > 0) {
@@ -1238,7 +1237,7 @@ exports.uploadPriceBook = async (req, res) => {
                   }
                 };
                 let option = { new: true }
-                let update = dealerPriceService.updateDealerPrice({ dealerId: req.body.dealerId, priceBook: priceBook._id }, newValue, option);
+                let update = dealerPriceService.updateDealerPrice({ dealerId: req.body.dealerId, priceBook: priceBook._id,status:true}, newValue, option);
                 let csvData = {
                   'priceBook': priceBook.name,
                   'status': 'Passed',
@@ -1249,7 +1248,7 @@ exports.uploadPriceBook = async (req, res) => {
               });
             });
 
-            upload = await dealerPriceService.uploadPriceBook(mergedArrayWithoutUndefined);
+            //upload = await dealerPriceService.uploadPriceBook(mergedArrayWithoutUndefined);
 
 
 
