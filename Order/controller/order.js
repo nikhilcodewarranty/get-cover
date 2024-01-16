78d5e674a917d327dde5563802f8f7a7fc067cbf
webdeveloper510 <webdeveloper510>
2024-01-16 16:12:30 +0530

    file validation check message ak

--- Order/controller/order.js
+++ Order/controller/order.js
@@ -6,6 +6,26 @@ const servicerService = require("../../Provider/services/providerService");
 const customerService = require("../../Customer/services/customerService");
 const priceBookService = require("../../PriceBook/services/priceBookService");
 const constant = require("../../config/constant");
+const multer = require('multer');
+const path = require('path');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const XLSX = require("xlsx");
+
+
+var StorageP = multer.diskStorage({
+    destination: function (req, files, cb) {
+        cb(null, path.join(__dirname, '../../uploads/orderFile'));
+    },
+    filename: function (req, files, cb) {
+        cb(null, files.fieldname + '-' + Date.now() + path.extname(files.originalname))
+    }
+})
+
+var uploadP = multer({
+    storage: StorageP,
+}).single('file');
+
+
 
 exports.createOrder = async (req, res) => {
     try {
@@ -73,18 +93,18 @@ exports.createOrder = async (req, res) => {
             }
         }
         data.createdBy = req.userId
-         let savedResponse =  await orderService.addOrder(data);
-         if(!savedResponse){
+        let savedResponse = await orderService.addOrder(data);
+        if (!savedResponse) {
             res.send({
-                code:constant.errorCode,
-                message:"unable to create order"
+                code: constant.errorCode,
+                message: "unable to create order"
             });
             return;
-         }
+        }
         res.send({
             code: constant.successCode,
             message: "Success",
-            result:savedResponse
+            result: savedResponse
         })
     } catch (err) {
         res.send({
@@ -93,3 +113,63 @@ exports.createOrder = async (req, res) => {
         })
     }
 };
+
+exports.checkFileValidation = async (req, res) => {
+    try {
+        uploadP(req, res, async (err) => {
+            let data = req.body
+            let file = req.file
+
+            let csvName = req.file.filename
+            const csvWriter = createCsvWriter({
+                path: './uploads/resultFile/' + csvName,
+                header: [
+                    { id: 'Brand', title: 'Brand' },
+                    { id: 'Model', title: 'Model' },
+                    { id: 'Serial', title: 'Serial' },
+                    { id: 'Class', title: 'Class' },
+                    { id: 'Condition', title: 'Condition' },
+                    { id: 'Retail Value', title: 'Retail Value' },
+                    // Add more headers as needed
+                ],
+            });
+            const wb = XLSX.readFile(req.file.path);
+            const sheets = wb.SheetNames;
+            const ws = wb.Sheets[sheets[0]];
+            const totalDataComing1 = XLSX.utils.sheet_to_json(wb.Sheets[sheets[0]]);
+
+            const headers = [];
+            for (let cell in ws) {
+              // Check if the cell is in the first row and has a non-empty value
+              if (/^[A-Z]1$/.test(cell) && ws[cell].v !== undefined && ws[cell].v !== null && ws[cell].v.trim() !== '') {
+                headers.push(ws[cell].v);
+              }
+            }
+      
+            if (headers.length !== 6) {
+              res.send({
+                code: constant.errorCode,
+                message: "Invalid file format detected. The sheet should contain exactly six columns."
+              })
+              return
+            }
+
+            const isValidLength = totalDataComing1.every(obj => Object.keys(obj).length === 6);
+            if(isValidLength){
+                console.log('valid length ---------------')
+            }else{
+                console.log('not valid length ---------------')
+            }
+            console.log('check+++++++++++++++++++++++++++',totalDataComing1,totalDataComing1.length,isValidLength)
+
+
+
+        })
+
+    } catch (err) {
+        res.send({
+            code: constant.errorCode,
+            message: err.message
+        })
+    }
+}
--- Order/routes/order.js
+++ Order/routes/order.js
@@ -4,6 +4,7 @@ const orderController = require("../controller/order");
 const { verifyToken } = require('../../middleware/auth'); // authentication with jwt as middleware
 
 router.post('/createOrder',[verifyToken],orderController.createOrder)
+router.post('/checkFileValidation',[verifyToken],orderController.checkFileValidation)
 
 
 
