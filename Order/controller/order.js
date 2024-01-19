2e3e22baa7005f52e887181b96402c1cafd7acc6
webdeveloper510 <developerweb6@gmail.com>
2024-01-19 21:52:35 +0530

    order csv upload am

--- Order/controller/order.js
+++ Order/controller/order.js
@@ -42,56 +42,56 @@ var uploadP = multer({
 exports.createOrder = async (req, res) => {
     try {
         upload(req, res, async (err) => {
-            let data = req.body
-            // let data = {
-            //     "dealerId": "65a0d25d503003dcd4abfc33",
-            //     "servicerId": "65a0d64b23eec30f66ea0c44",
-            //     "customerId": "65a0e563169e80fd0600a965",
-            //     "productsArray": [
-            //         {
-            //             "categoryId": "65a0dacd3a9009fd982ba41e",
-            //             "priceBookId": "65a0daf83a9009fd982ba41f",
-            //             "unitPrice": "80.00",
-            //             "noOfProducts": "",
-            //             "price": 160,
-            //             "file": "",
-            //             "manufacture": "Get-Cover123",
-            //             "model": "Inverter123",
-            //             "serial": "S123GHK",
-            //             "condition": "Breakdown",
-            //             "productValue": 123,
-            //             "regDate": "2024-01-18T00:00:00.000Z",
-            //             "coverageStartDate": "2024-01-30T00:00:00.000Z",
-            //             "coverageEndDate": "2025-01-30T00:00:00.000Z",
-            //             "description": "003",
-            //             "term": 12,
-            //             "priceType": "Quantity Pricing",
-            //             "additionalNotes": "this is test ",
-            //             "QuantityPricing": [
-            //                 {
-            //                     "name": "a",
-            //                     "quantity": 45,
-            //                     "_id": "65a7863cc6690cd3e0a62256",
-            //                     "enterQuantity": "20"
-            //                 },
-            //                 {
-            //                     "name": "b",
-            //                     "quantity": 10,
-            //                     "_id": "65a7863cc6690cd3e0a62257",
-            //                     "enterQuantity": "11"
-            //                 }
-            //             ]
-            //         }
-            //     ],
-            //     "sendNotification": true,
-            //     "paymentStatus": "Paid",
-            //     "dealerPurchaseOrder": "#12345",
-            //     "serviceCoverageType": "Parts",
-            //     "coverageType": "Breakdown",
-            //     "orderAmount": 144,
-            //     "paidAmount": 123,
-            //     "dueAmount": 21
-            // }
+            // let data = req.body
+            let data = {
+                "dealerId": "65a0d25d503003dcd4abfc33",
+                "servicerId": "65a0d64b23eec30f66ea0c44",
+                "customerId": "65a0e563169e80fd0600a965",
+                "productsArray": [
+                    {
+                        "categoryId": "65a0dacd3a9009fd982ba41e",
+                        "priceBookId": "65a0daf83a9009fd982ba41f",
+                        "unitPrice": "80.00",
+                        "noOfProducts": "",
+                        "price": 160,
+                        "file": "",
+                        "manufacture": "Get-Cover123",
+                        "model": "Inverter123",
+                        "serial": "S123GHK",
+                        "condition": "Breakdown",
+                        "productValue": 123,
+                        "regDate": "2024-01-18T00:00:00.000Z",
+                        "coverageStartDate": "2024-01-30T00:00:00.000Z",
+                        "coverageEndDate": "2025-01-30T00:00:00.000Z",
+                        "description": "003",
+                        "term": 12,
+                        "priceType": "Quantity Pricing",
+                        "additionalNotes": "this is test ",
+                        "QuantityPricing": [
+                            {
+                                "name": "a",
+                                "quantity": 45,
+                                "_id": "65a7863cc6690cd3e0a62256",
+                                "enterQuantity": "20"
+                            },
+                            {
+                                "name": "b",
+                                "quantity": 10,
+                                "_id": "65a7863cc6690cd3e0a62257",
+                                "enterQuantity": "11"
+                            }
+                        ]
+                    }
+                ],
+                "sendNotification": true,
+                "paymentStatus": "Paid",
+                "dealerPurchaseOrder": "#12345",
+                "serviceCoverageType": "Parts",
+                "coverageType": "Breakdown",
+                "orderAmount": 144,
+                "paidAmount": 123,
+                "dueAmount": 21
+            }
 
             if (req.role != "Super Admin") {
                 res.send({
@@ -200,12 +200,12 @@ exports.createOrder = async (req, res) => {
                 const totalDataComing1 = XLSX.utils.sheet_to_json(ws);
                 let contractObject = {
                     orderId: savedResponse._id,
-                    productName: priceBook[0].name,
+                    productName: 'asdas',
                     manufacture: totalDataComing1[0]['Brand'],
                     model: totalDataComing1[0]['Model'],
                     serial: totalDataComing1[0]['Serial'],
                     condition: totalDataComing1[0]['Condition'],
-                    productValue: totalDataComing1[0]['Retail Value'],
+                    productValue: totalDataComing1[0]['Retail'],
                     unique_key: contractCount
 
                 }
