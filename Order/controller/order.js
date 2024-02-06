753861d7dc4e8fd5a9efa12822d03645ab602b4c
webdeveloper510 <developerweb6@gmail.com>
2024-02-06 15:14:31 +0530

    upload validation

--- Order/controller/order.js
+++ Order/controller/order.js
@@ -121,8 +121,10 @@ exports.createOrder = async (req, res) => {
             // console.log("Body=================",hhhhh)
             // console.log("productsArray=================",data.productsArray[0].QuantityPricing)
             for(let i=0; i < data.productsArray.length; i++){
-                let jsonArray = JSON.parse(data.productsArray[i].QuantityPricing);
-                data.productsArray[i].QuantityPricing = jsonArray
+                if(data.productsArray[i].QuantityPricing){
+                    let jsonArray = JSON.parse(data.productsArray[i].QuantityPricing);
+                    data.productsArray[i].QuantityPricing = jsonArray
+                }
             }
             // console.log("new array=================",data.productsArray);
             // return;
