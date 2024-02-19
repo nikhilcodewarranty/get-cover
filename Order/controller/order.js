b5ef0002e7a34b6f375a3e9edbf1a11eb1be702a
webdeveloper510 <developerweb6@gmail.com>
2024-02-19 15:08:59 +0530

    image

--- Order/controller/order.js
+++ Order/controller/order.js
@@ -2733,7 +2733,7 @@ exports.generatePDF = async (req, res) => {
             <tbody>
                 <tr>
                     <td style="text-align: left; width: 50%;">
-                        <img src='http://15.207.221.207:3002/uploads/logo.png' style="margin-bottom: 20px;"/>
+                        <img src='http://15.207.221.207/static/media/logo.642c96aed42bd8a1d454.png' style="margin-bottom: 20px;"/>
                         <h1 style="margin: 0; padding: 0; font-size:20px"><b>Get Cover </b></h1>
                         <p style="margin: 0; padding: 0;">13th Street <br/>
                         47 W 13th St, New York,<br/>
