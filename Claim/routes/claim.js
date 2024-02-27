042e0c0d8328301421cb122c7901e635a7e67be7
webdeveloper510 <developerweb6@gmail.com>
2024-02-27 23:23:50 +0530

    Create claim and get all claim

--- Claim/routes/claim.js
+++ Claim/routes/claim.js
@@ -6,6 +6,7 @@ router.post("/searchClaim", [verifyToken],claimController.searchClaim);
 router.post("/uploadReceipt", [verifyToken],claimController.uploadReceipt);
 router.post("/createClaim",[verifyToken],claimController.addClaim)
 router.post("/getAllClaims",[verifyToken],claimController.getAllClaims)
+router.get("/getContractById/:contractId",[verifyToken],claimController.getContractById)
 
 
 module.exports = router;
