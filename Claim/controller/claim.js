b25886f17f5dc30c1f38fbc8e7fa0cf5b3913b07
webdeveloper510 <developerweb6@gmail.com>
2024-02-28 00:45:17 +0530

    change resller status am

--- Claim/controller/claim.js
+++ Claim/controller/claim.js
@@ -171,9 +171,25 @@ exports.searchClaim = async (req, res, next) => {
           ]
         },
       },
-      
-      { $skip: skipLimit },
-      { $limit: pageLimit },
+      {
+        $facet: {
+          totalRecords: [
+            {
+              $count: "total"
+            }
+          ],
+          data: [
+            {
+              $skip: skipLimit
+            },
+            {
+              $limit: pageLimit
+            }
+          ]
+        }
+      }, 
+      // { $skip: skipLimit },
+      // { $limit: pageLimit },
     ]
 
     let query2 = [
