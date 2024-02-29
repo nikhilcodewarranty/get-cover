fca00c7831e34a04f8f94109683550a1d29e6d37
webdeveloper510 <webdeveloper510>
2024-02-29 14:37:13 +0530

    contract ID ak

--- Order/controller/order.js
+++ Order/controller/order.js
@@ -584,7 +584,7 @@ exports.createOrder1 = async (req, res) => {
                         productValue: data.retailValue,
                         unique_key: unique_key1,
                         unique_key_search: unique_key_search1,
-                        unique_key_number: unique_key_number1,
+                        unique_key_number: contractIncrease,
                     };
                     contractArray.push(contractObject);
                     contractIncrease = 0
