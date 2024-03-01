3b88c8fd46a1b558faf9cd058d136d2b5d64c336
webdeveloper510 <webdeveloper510>
2024-03-01 11:33:53 +0530

    pull origin ak

--- Claim/model/claim.js
+++ Claim/model/claim.js
@@ -114,6 +114,10 @@ const claimSchema = new mongoose.Schema({
     default: 0
     // required: true,
   },
+  note:{
+    type:String,
+    default:''
+  }
 }, { timestamps: true });
 
 module.exports = mongoose.model("claim", claimSchema);
