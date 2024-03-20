1936fa3ec0a2d6213abf6ec1771ead49445a81bd
webdeveloper510 <developerweb6@gmail.com>
2024-03-20 21:02:02 +0530

    claims issue

--- Claim/controller/claim.js
+++ Claim/controller/claim.js
@@ -807,8 +807,9 @@ exports.getAllClaims = async (req, res, next) => {
       }
       if (item1.servicerId != null) {
         servicerName = servicer.find(servicer => servicer._id.toString() === item1.servicerId.toString());
-        console.log("req.userId-------",req.userId)
-        selfServicer = item1.servicerId.toString() === req.userId.toString() ? true : false
+        console.log("req.userId-------", req.userId)
+        const userId = req.userId ? req.userId : '65f01eed2f048cac854daaa5'
+        selfServicer = item1.servicerId.toString() === userId.toString() ? true : false
       }
       return {
         ...item1,
