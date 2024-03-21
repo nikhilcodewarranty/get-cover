b7663c05e67b127e714736460413c70d44346fa5
webdeveloper510 <developerweb6@gmail.com>
2024-03-21 20:59:37 +0530

    claims dashboard api

--- Contract/controller/contracts.js
+++ Contract/controller/contracts.js
@@ -106,12 +106,12 @@ exports.getAllContracts = async (req, res) => {
         {
           $and: [
             // { unique_key: { $regex: `^${data.contractId ? data.contractId : ''}` } },
-            { unique_key: { '$regex': data.contractId ? data.contractId : '', '$options': 'i' } },
-            { productName: { '$regex': data.productName ? data.productName : '', '$options': 'i' } },
-            { serial: { '$regex': data.serial ? data.serial : '', '$options': 'i' } },
-            { manufacture: { '$regex': data.manufacture ? data.manufacture : '', '$options': 'i' } },
-            { model: { '$regex': data.model ? data.model : '', '$options': 'i' } },
-            { status: { '$regex': data.status ? data.status : '', '$options': 'i' } },
+            { unique_key: { '$regex': data.contractId ? data.contractId.replace(/\s+/g, ' ').trim() : '', '$options': 'i' } },
+            { productName: { '$regex': data.productName ? data.productName.replace(/\s+/g, ' ').trim() : '', '$options': 'i' } },
+            { serial: { '$regex': data.serial ? data.serial.replace(/\s+/g, ' ').trim() : '', '$options': 'i' } },
+            { manufacture: { '$regex': data.manufacture ? data.manufacture.replace(/\s+/g, ' ').trim() : '', '$options': 'i' } },
+            { model: { '$regex': data.model ? data.model.replace(/\s+/g, ' ').trim() : '', '$options': 'i' } },
+            { status: { '$regex': data.status ? data.status.replace(/\s+/g, ' ').trim() : '', '$options': 'i' } },
             // { eligibility: true },
           ]
         },
@@ -135,9 +135,9 @@ exports.getAllContracts = async (req, res) => {
         $match:
         {
           $and: [
-            { "order.venderOrder": { '$regex': data.venderOrder ? data.venderOrder : '', '$options': 'i' } },
+            { "order.venderOrder": { '$regex': data.venderOrder ? data.venderOrder.replace(/\s+/g, ' ').trim() : '', '$options': 'i' } },
             // { "order.unique_key": { $regex: `^${data.orderId ? data.orderId : ''}` } },
-            { "order.unique_key": { '$regex': data.orderId ? data.orderId : '', '$options': 'i' } },
+            { "order.unique_key": { '$regex': data.orderId ? data.orderId.replace(/\s+/g, ' ').trim() : '', '$options': 'i' } },
           ]
         },
 
