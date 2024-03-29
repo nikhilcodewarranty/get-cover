30eadf0a445e404146999ceeed4034d1e22f6079
webdeveloper510 <webdeveloper510>
2024-03-29 17:11:11 +0530

    servicer name ak

--- Provider/controller/serviceAdminController.js
+++ Provider/controller/serviceAdminController.js
@@ -386,7 +386,7 @@ exports.editServicerDetail = async (req, res) => {
       return;
     }
     if (data.name != data.oldName) {
-      let checkName = await providerService.getServicerByName({ name: new RegExp(data.name) }, {})
+      let checkName = await providerService.getServicerByName({ name: { $regex: data.name } }, {})
       if (checkName) {
         res.send({
           code: constant.errorCode,
