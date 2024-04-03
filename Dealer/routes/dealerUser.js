557417da0c2fa8163416f2ac6b27047eb86c48db
webdeveloper510 <developerweb6@gmail.com>
2024-04-03 14:31:00 +0530

    update delaer

--- Dealer/routes/dealerUser.js
+++ Dealer/routes/dealerUser.js
@@ -4,42 +4,42 @@ const dealerUserController = require("../controller/dealerUserController");
 const { verifyToken } = require('../../middleware/auth'); // authentication with jwt as middleware
 const validator = require('../config/validation');
 
-router.post('/getDealerUsers',[verifyToken],dealerUserController.getDealerUsers)
+router.post('/getDealerUsers', [verifyToken], dealerUserController.getDealerUsers)
 router.post('/createOrder', [verifyToken], dealerUserController.createOrder)
-router.post('/editOrderDetail/:orderId', [verifyToken], dealerUserController.editOrderDetail)
-router.post('/getPriceBooks',[verifyToken],dealerUserController.getPriceBooks)
-router.post('/createDealerPriceBook',[verifyToken],dealerUserController.createDealerPriceBook)
-router.post('/getAllPriceBooksByFilter',[verifyToken],dealerUserController.getAllPriceBooksByFilter)
-router.post('/createCustomer',[verifyToken],dealerUserController.createCustomer)
-router.post('/createDealerPriceBook',[verifyToken],dealerUserController.createDealerPriceBook)
-router.post('/getDealerCustomers',[verifyToken],dealerUserController.getDealerCustomers)
-router.put("/updateDealerPriceBook/:dealerPriceBookId", [verifyToken], dealerUserController.statusUpdate); 
-router.post("/getResellerPriceBook/:resellerId", [verifyToken], dealerUserController.getResellerPriceBook); 
+router.post('/editOrderDetail/:orderId', [verifyToken], dealerUserController.editOrderDetail) 
+router.post('/getPriceBooks', [verifyToken], dealerUserController.getPriceBooks)
+router.post('/createDealerPriceBook', [verifyToken], dealerUserController.createDealerPriceBook)
+router.post('/getAllPriceBooksByFilter', [verifyToken], dealerUserController.getAllPriceBooksByFilter)
+router.post('/createCustomer', [verifyToken], dealerUserController.createCustomer)
+router.post('/createDealerPriceBook', [verifyToken], dealerUserController.createDealerPriceBook)
+router.post('/getDealerCustomers', [verifyToken], dealerUserController.getDealerCustomers)
+router.put("/updateDealerPriceBook/:dealerPriceBookId", [verifyToken], dealerUserController.statusUpdate);
+router.post("/getResellerPriceBook/:resellerId", [verifyToken], dealerUserController.getResellerPriceBook);
 router.post('/getCustomerInOrder', [verifyToken], dealerUserController.getCustomerInOrder)
 router.post('/getServicerInOrders', [verifyToken], dealerUserController.getServicerInOrders)
 router.get('/getDashboardData', [verifyToken], dealerUserController.getDashboardData)
 
-router.post('/getDealerResellers',[verifyToken],dealerUserController.getDealerResellers)
-router.post('/getDealerResellersInOrder',[verifyToken],dealerUserController.getDealerResellersInOrder)
-router.get('/getDealerPriceBookById/:dealerPriceBookId',[verifyToken],dealerUserController.getDealerPriceBookById)
-router.post('/getCustomerOrders/:customerId',[verifyToken],dealerUserController.customerOrders)
-router.post('/getResellerOrders/:resellerId',[verifyToken],dealerUserController.getResellerOrders)
+router.post('/getDealerResellers', [verifyToken], dealerUserController.getDealerResellers)
+router.post('/getDealerResellersInOrder', [verifyToken], dealerUserController.getDealerResellersInOrder)
+router.get('/getDealerPriceBookById/:dealerPriceBookId', [verifyToken], dealerUserController.getDealerPriceBookById)
+router.post('/getCustomerOrders/:customerId', [verifyToken], dealerUserController.customerOrders)
+router.post('/getResellerOrders/:resellerId', [verifyToken], dealerUserController.getResellerOrders)
 router.post("/getResellerUsers/:resellerId", [verifyToken], dealerUserController.getResellerUsers);
 router.post("/getResellerServicers/:resellerId", [verifyToken], dealerUserController.getResellerServicers)
 router.post("/getResellerCustomers/:resellerId", [verifyToken], dealerUserController.getResellerCustomers);
-router.post('/createReseller',[verifyToken],dealerUserController.createReseller)
+router.post('/createReseller', [verifyToken], dealerUserController.createReseller)
 router.post('/getCategoryAndPriceBooks', [verifyToken], dealerUserController.getCategoryAndPriceBooks)
-router.post('/getDealerServicers',[verifyToken],dealerUserController.getDealerServicers)
-router.post('/getDealerContracts',[verifyToken],dealerUserController.getAllContracts)
+router.post('/getDealerServicers', [verifyToken], dealerUserController.getDealerServicers)
+router.post('/getDealerContracts', [verifyToken], dealerUserController.getAllContracts)
 
-router.get('/getServicersList',[verifyToken],dealerUserController.getServicersList)
-router.post('/createDeleteRelation',[verifyToken],dealerUserController.createDeleteRelation)
+router.get('/getServicersList', [verifyToken], dealerUserController.getServicersList)
+router.post('/createDeleteRelation', [verifyToken], dealerUserController.createDeleteRelation)
 //router.post('/getArchieveOrder', [verifyToken], dealerUserController.getAllArchieveOrders)
-router.post('/getDealerOrders',[verifyToken],dealerUserController.getDealerOrders)
-router.post('/getDealerArchievedOrders',[verifyToken],dealerUserController.getDealerArchievedOrders)
+router.post('/getDealerOrders', [verifyToken], dealerUserController.getDealerOrders)
+router.post('/getDealerArchievedOrders', [verifyToken], dealerUserController.getDealerArchievedOrders)
 
-router.post("/createClaim",[verifyToken],dealerUserController.addClaim)
+router.post("/createClaim", [verifyToken], dealerUserController.addClaim)
 
-router.post("/getAllClaims",[verifyToken],dealerUserController.getAllClaims)
+router.post("/getAllClaims", [verifyToken], dealerUserController.getAllClaims)
 
 module.exports = router; 
