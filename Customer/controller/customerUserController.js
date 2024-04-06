7d7afc2767f5387c041a9a1e3984ef1aac73d43b
webdeveloper510 <webdeveloper510>
2024-04-06 18:16:06 +0530

    updated codes ak

--- Customer/controller/customerUserController.js
+++ Customer/controller/customerUserController.js
@@ -583,7 +583,7 @@ exports.getCustomerContract = async (req, res) => {
           { manufacture: { '$regex': data.manufacture ? data.manufacture.replace(/\s+/g, ' ').trim() : '', '$options': 'i' } },
           { model: { '$regex': data.model ? data.model.replace(/\s+/g, ' ').trim() : '', '$options': 'i' } },
           { status: { '$regex': data.status ? data.status.replace(/\s+/g, ' ').trim() : '', '$options': 'i' } },
-          { eligibility: data.eligibilty === "true" ? true : false },
+          { eligibilty: data.eligibilty === "true" ? true : false },
         ]
       } else {
         contractFilter = [
--- Dealer/controller/dealerController.js
+++ Dealer/controller/dealerController.js
@@ -3125,7 +3125,7 @@ exports.getDealerContract = async (req, res) => {
         { manufacture: { '$regex': data.manufacture ? data.manufacture.replace(/\s+/g, ' ').trim() : '', '$options': 'i' } },
         { model: { '$regex': data.model ? data.model.replace(/\s+/g, ' ').trim() : '', '$options': 'i' } },
         { status: { '$regex': data.status ? data.status.replace(/\s+/g, ' ').trim() : '', '$options': 'i' } },
-        { eligibility: data.eligibilty === "true" ? true : false },
+        { eligibilty: data.eligibilty === "true" ? true : false },
       ]
     } else {
       contractFilter = [
