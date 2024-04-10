701c296f19a629808f49cf1ded12f17d0db541e0
webdeveloper510 <developerweb6@gmail.com>
2024-04-10 19:27:19 +0530

    trim space

--- Dealer/controller/dealerController.js
+++ Dealer/controller/dealerController.js
@@ -3114,6 +3114,20 @@ exports.getDealerContract = async (req, res) => {
             {
               $limit: pageLimit
             },
+            // {
+            //   $project: {
+            //     productName: 1,
+            //     model: 1,
+            //     serial: 1,
+            //     unique_key: 1,
+            //     status: 1,
+            //     manufacture: 1,
+            //     eligibilty: 1,
+            //     "order.unique_key": 1,
+            //     "order.venderOrder": 1,
+            //     "order.dealerId": 1,
+            //   }
+            // }
             {
               $project: {
                 productName: 1,
@@ -3123,9 +3137,13 @@ exports.getDealerContract = async (req, res) => {
                 status: 1,
                 manufacture: 1,
                 eligibilty: 1,
-                "order.unique_key": 1,
-                "order.venderOrder": 1,
-                "order.dealerId": 1,
+                order_unique_key: { $arrayElemAt: ["$order.unique_key", 0] },
+                order_venderOrder: { $arrayElemAt: ["$order.venderOrder", 0] },
+                order: {
+                  unique_key: { $arrayElemAt: ["$order.unique_key", 0] },
+                  venderOrder: { $arrayElemAt: ["$order.venderOrder", 0] },
+                },
+                totalRecords: 1
               }
             }
           ],
@@ -3175,12 +3193,12 @@ exports.getDealerContract = async (req, res) => {
           as: "order",
         }
       },
-      {
-        $unwind: {
-          path: "$order",
-          preserveNullAndEmptyArrays: true,
-        }
-      },
+      // {
+      //   $unwind: {
+      //     path: "$order",
+      //     preserveNullAndEmptyArrays: true,
+      //   }
+      // },
       {
         $match:
         {
