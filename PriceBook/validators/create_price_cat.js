b901a9fcce7c79965bb077123ffd54f9ae2d027e
webdeveloper510 <webdeveloper510>
2024-04-19 17:52:37 +0530

    coverage type ak

--- PriceBook/validators/create_price_cat.js
+++ PriceBook/validators/create_price_cat.js
@@ -3,7 +3,6 @@ const Joi = require('joi')
 const create_price_cat_validation = Joi.object({
     name:Joi.string().trim().required(),
     description:Joi.string().trim().required(),
-    coverageType:Joi.string().trim().required(),
     status: Joi.boolean().optional()
 });
 module.exports = create_price_cat_validation
