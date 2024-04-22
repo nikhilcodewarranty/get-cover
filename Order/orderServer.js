8c8255917801339840708948b97b14bf5243c2fd
webdeveloper510 <developerweb6@gmail.com>
2024-04-22 22:12:05 +0530

    claim changes

--- Order/orderServer.js
+++ Order/orderServer.js
@@ -34,6 +34,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/uploads/', express.static('./uploads'))
+app.use('/Order/mergedFile/', express.static('./Order/mergedFile/'))
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
