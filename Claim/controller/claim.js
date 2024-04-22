69189e7592441d209bf9eb868df584d188bfed49
webdeveloper510 <developerweb6@gmail.com>
2024-04-22 19:44:19 +0530

    claim changes

--- Claim/controller/claim.js
+++ Claim/controller/claim.js
@@ -2039,7 +2039,7 @@ exports.saveBulkClaim = async (req, res) => {
           return null;
         }
       })
-      // res.json(totalDataComing);
+      // res.json(totalDataComing); 
       // return;
       const updateArray = await Promise.all(updateArrayPromise);
       totalDataComing.map((data, index) => {
@@ -2115,7 +2115,7 @@ exports.saveBulkClaim = async (req, res) => {
       } 
 
       const htmlTableString = convertArrayToHTMLTable(csvArray);
-      const mailing = sgMail.send(emailConstant.sendCsvFile('yashasvi@codenomad.net', htmlTableString));
+      const mailing = sgMail.send(emailConstant.sendCsvFile('amit@codenomad.net', htmlTableString));
 
       res.send({
         code: constant.successCode,
