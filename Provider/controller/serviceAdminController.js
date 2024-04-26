413750b2ce6d7c13101ccf8671eeccc064da576b
webdeveloper510 <developerweb6@gmail.com>
2024-04-26 14:24:11 +0530

    shd fh

--- Provider/controller/serviceAdminController.js
+++ Provider/controller/serviceAdminController.js
@@ -1803,7 +1803,8 @@ exports.paidUnpaidClaim = async (req, res) => {
       if (item1.servicerId != null) {
         servicerName = servicer.find(servicer => servicer._id.toString() === item1.servicerId.toString());
         const userId = req.userId ? req.userId : '65f01eed2f048cac854daaa5'
-        selfServicer = item1.servicerId.toString() === userId.toString() ? true : false
+       // selfServicer = item1.servicerId.toString() === userId.toString() ? true : false
+        selfServicer = item1.servicerId?.toString() === item1.servicerData?._id?.toString() && item1.servicerData?.isServicer ? true : false
       }
       return {
         ...item1,
