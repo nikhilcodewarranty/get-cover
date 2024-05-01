17ee3ed8c5baa3d3d0aa2c9ec982a783bfcb4f7e
webdeveloper510 <developerweb6@gmail.com>
2024-05-01 16:33:13 +0530

    check servicer update

--- Customer/controller/customerController.js
+++ Customer/controller/customerController.js
@@ -12,7 +12,7 @@ const constant = require("../../config/constant");
 const { default: mongoose } = require("mongoose");
 const serviceProvider = require("../../Provider/model/serviceProvider");
 const emailConstant = require('../../config/emailConstant');
-const randtoken = require('rand-token').generator()
+const randtoken = require('rand-token').generator() 
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.sendgrid_key);
 exports.createCustomer = async (req, res, next) => {
