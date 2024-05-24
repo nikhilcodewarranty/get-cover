cbf894cfb6c3c24f6643f7e88c1f480af36b8012
webdeveloper510 <webdeveloper510>
2024-05-24 12:35:37 +0530

    check ak

--- Dealer/controller/dealerUserController.js
+++ Dealer/controller/dealerUserController.js
@@ -310,13 +310,13 @@ exports.getPriceBooks = async (req, res) => {
 
         }
         let query
-        if (checkDealer[0]?.coverageType != "Breakdown & Accidental") {
+        if (checkDealer[0]?.coverageType == "Breakdown & Accidental") {
             query = { isDeleted: false, status: true, dealerId: new mongoose.Types.ObjectId(req.userId) }
         } else {
             query = { isDeleted: false, status: true, coverageType: checkDealer[0]?.coverageType, dealerId: new mongoose.Types.ObjectId(req.userId) }
 
         }
-        console.log('skldjflksjdf', checkDealer)
+        console.log('skldjflksjdf',query, checkDealer)
         let lookupQuery
         if (checkDealer[0]?.coverageType != "Breakdown & Accidental") {
             lookupQuery = [
