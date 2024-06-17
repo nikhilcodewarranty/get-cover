b7b5bcd7a703213dd07f9c57f27a3535d5b6c04b
webdeveloper510 <webdeveloper510>
2024-06-17 09:59:11 +0530

    check ak

--- User/model/users.js
+++ User/model/users.js
@@ -17,7 +17,8 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     default: '',
-    index: true
+    index: true,
+    lowercase: true
   },
   password: {
     type: String,
