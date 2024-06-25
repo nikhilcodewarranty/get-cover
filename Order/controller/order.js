3a3ad1d4b76ec31f91f4e5022a2624cfaa38d9b6
webdeveloper510 <developerweb6@gmail.com>
2024-06-25 18:55:35 +0530

    check am

--- Order/controller/order.js
+++ Order/controller/order.js
@@ -3902,9 +3902,9 @@ exports.editOrderDetail = async (req, res) => {
             //Send email to customer with term and condtion
             //generate T anc C
             console.log("checkDealer-------------------------", checkDealer)
-            console.log("savedResponse-------------------------", savedResponse)
+            console.log("savedResponse-------------------------", checkOrder)
             if (checkDealer?.termCondition) {
-                const tcResponse = await generateTC(updatePaidDate);
+                const tcResponse = await generateTC(checkOrder);
             }
             // reporting codes
             logData.response = {
