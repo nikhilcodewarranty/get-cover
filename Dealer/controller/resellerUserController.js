0cde4a979a64e6b2ac137f4b5204f3a9457078b7
webdeveloper510 <developerweb6@gmail.com>
2024-06-27 16:36:10 +0530

    get cateory in reseller ak user

--- Dealer/controller/resellerUserController.js
+++ Dealer/controller/resellerUserController.js
@@ -1350,6 +1350,13 @@ exports.getCategoryAndPriceBooks = async (req, res) => {
             pName: pName,
         }));
 
+        let priceBookDetail
+        if (mergedPriceBooks.length == 1) {
+             priceBookDetail = mergedPriceBooks[0]
+        } else {
+            priceBookDetail = {}
+        }
+
         let result = {
             priceCategories: getCategories,
             priceBooks: data.priceCatId == "" ? [] : mergedPriceBooks,
@@ -1357,6 +1364,7 @@ exports.getCategoryAndPriceBooks = async (req, res) => {
             terms: data.priceCatId == "" ? [] : uniqueTerms,
             selectedCategory: checkSelectedCategory ? checkSelectedCategory : "",
             dealerPriceBookDetail: dealerPriceBookDetail,
+            priceBookDetail
         };
 
         res.send({
