7044ac76d169fcb880536a8f2dd2e4ba0e7467ed
webdeveloper510 <developerweb6@gmail.com>
2024-07-04 16:38:14 +0530

    search api check ak

--- Dealer/controller/resellerController.js
+++ Dealer/controller/resellerController.js
@@ -1269,7 +1269,7 @@ exports.getResellerOrders = async (req, res) => {
             { $sort: { unique_key: -1 } }
         ]
 
-        let pageLimit = data.pageLimit ? Number(data.pageLimit) : 100
+        let pageLimit = data.pageLimit ? Number(data.pageLimit) : 1000000
         let skipLimit = data.page > 0 ? ((Number(req.body.page) - 1) * Number(pageLimit)) : 0
         let limitData = Number(pageLimit)
 
