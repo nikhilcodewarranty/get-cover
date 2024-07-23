6d35c038ac9e0a087fa10546c48ac617af413cba
webdeveloper510 <developerweb6@gmail.com>
2024-07-23 16:22:28 +0530

    push to the master branch 23 july

--- User/controller/usersController.js
+++ User/controller/usersController.js
@@ -2736,7 +2736,7 @@ exports.addMembers = async (req, res) => {
     let resetPasswordCode = randtoken.generate(4, '123456789')
     let checkPrimaryEmail2 = await userService.updateSingleUser({ email: data.email }, { resetPasswordCode: resetPasswordCode }, { new: true });
     let resetLink = `${process.env.SITE_URL}newPassword/${checkPrimaryEmail2._id}/${resetPasswordCode}`
-    const resetPassword = sgMail.send(emailConstant.servicerApproval(data.email, { link: resetLink, role: req.role, servicerName: data.firstName }))
+    const resetPassword = sgMail.send(emailConstant.servicerApproval(data.email, { link: resetLink, role: req.role == 'Super Admin' ? 'Admin' : req.role, servicerName: data.firstName }))
     // // Create the user
     res.send({
       code: constant.successCode,
