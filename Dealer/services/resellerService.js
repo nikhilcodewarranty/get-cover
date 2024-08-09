803a1f6a7f46c1fb8b49ca0ac7b064fea2f829e7
webdeveloper510 <developerweb6@gmail.com>
2024-08-09 19:30:31 +0530

    services refine, remove console from services 9 aug

--- Dealer/services/resellerService.js
+++ Dealer/services/resellerService.js
@@ -2,16 +2,16 @@ const reseller = require("../model/reseller")
 const user = require('../../User/model/users')
 
 module.exports = class resellerService {
-      // Create a new reseller
+    // Create a new reseller
     static async createReseller(data) {
         try {
-            let createReseller = await new reseller(data).save();
+            let createReseller = await new reseller(data).save(); 
             return createReseller;
         } catch (err) {
-            return `Could not create reseller: ${err}`; 
+            return `Could not create reseller: ${err}`;
         }
     }
-  // Retrieve resellers based on a query and projection
+    // Retrieve resellers based on a query and projection
     static async getResellers(query, projection) {
         try {
             let getResellers = await reseller.find(query, projection)
@@ -20,7 +20,7 @@ module.exports = class resellerService {
             return `Could not fetch reseller data: ${err}`;
         }
     }
-  // Retrieve a single reseller based on a query and projection
+    // Retrieve a single reseller based on a query and projection
     static async getReseller(query, projection) {
         try {
             let getResellers = await reseller.findOne(query, projection)
@@ -29,7 +29,7 @@ module.exports = class resellerService {
             return `Could not fetch reseller: ${err}`;
         }
     }
-  // Retrieve and sort resellers count based on a query
+    // Retrieve and sort resellers count based on a query
     static async getResellersCount(query) {
         try {
             const allReselers = await reseller.find(query).sort({ 'unique_key': -1 });
@@ -38,7 +38,7 @@ module.exports = class resellerService {
             return `Could not fetch resellers count: ${error}`;
         }
     }
-  // Retrieve resellers using aggregation
+    // Retrieve resellers using aggregation
     static async getResellerByAggregate(query) {
         try {
             const singleResellerResponse = await reseller.aggregate(query);
@@ -48,7 +48,7 @@ module.exports = class resellerService {
         }
 
     }
-  // Update multiple resellers based on a query
+    // Update multiple resellers based on a query
     static async updateMeta(query, projection) {
         try {
             let updateMeta = await reseller.updateMany(query, projection);
@@ -58,7 +58,7 @@ module.exports = class resellerService {
             return `Could not update meta data: ${err}`;
         }
     }
-  // Update a single reseller based on criteria
+    // Update a single reseller based on criteria
     static async updateReseller(criteria, data) {
         try {
             let updateMeta = await reseller.findOneAndUpdate(criteria, data, { new: true });
@@ -67,5 +67,5 @@ module.exports = class resellerService {
             return `Could not update reseller: ${err}`;
         }
     }
- 
+
 }
