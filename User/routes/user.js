ac33343e5ca8f2a33493fa8c2ad33c8170b47e79
webdeveloper510 <webdeveloper510>
2024-08-22 19:05:07 +0530

    check ak

--- User/routes/user.js
+++ User/routes/user.js
@@ -42,7 +42,7 @@ router.put("/rejectDealer/:dealerId", [verifyToken], validator("approve_reject_d
 router.put("/updateUserData/:userId", [verifyToken], userController.updateUserData); // update user data
 router.put("/updateUser/:userId", [verifyToken], userController.updateUser); // update user
 router.post("/checkEmail", [verifyToken], validator("email_validation"), userController.checkEmail); // check email
-router.post("/downloadFile", [verifyToken], userController.downloadFile); // check email
+router.post("/downloadFile",  userController.downloadFile); // check email
 router.post("/validateData", [verifyToken], userController.validateData); // validate data
 router.get("/checkToken", [verifyToken], userController.checkToken); // check token
 router.get("/getAccountInfo", [verifyToken], userController.getAccountInfo); // get account info
