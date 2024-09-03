2b2956d61234aebc01e0e9608e818f7668b624df
webdeveloper510 <webdeveloper510>
2024-09-03 12:37:04 +0530

    upload logo change

--- index.js
+++ index.js
@@ -88,23 +88,23 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/uploads/logo/:filename', express.static('./uploads/'))
+app.use('/uploads/', express.static('./uploads/'))
 
-app.get('/uploads/logo/:filename', (req, res) => {
-  const folder = 'logo';
-  const filename = req.params.filename;
-  const filePath = path.join(__dirname, 'uploads', folder, filename);
+// app.get('/uploads/logo/:filename', (req, res) => {
+//   const folder = 'logo';
+//   const filename = req.params.filename;
+//   const filePath = path.join(__dirname, 'uploads', folder, filename);
 
-  // Check if the file exists
-  fs.access(filePath, fs.constants.F_OK, (err) => {
-    if (err) {
-      return res.status(404).send('File not found');
-    }
+//   // Check if the file exists
+//   fs.access(filePath, fs.constants.F_OK, (err) => {
+//     if (err) {
+//       return res.status(404).send('File not found');
+//     }
 
-    // Send the file if it exists
-    res.sendFile(filePath);
-  });
-});
+//     // Send the file if it exists
+//     res.sendFile(filePath);
+//   });
+// });
 
 
 var cron = require('node-cron');
