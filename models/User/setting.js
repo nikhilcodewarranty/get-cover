85d52a630f34d077cab5769b8f599cf4385b22ba
webdeveloper510 <developerweb6@gmail.com>
2024-09-05 16:47:07 +0530

    rget setting

--- models/User/setting.js
+++ models/User/setting.js
@@ -2,60 +2,7 @@ const mongoose = require("mongoose");
 const connection = require('../../db');
 
 // Define the default resetColor array
-const defaultResetColor = [
-  {
-    colorCode: "#303030",
-    colorType: "sideBarColor"
-  },
-  {
-    colorCode: "#fafafa",
-    colorType: "sideBarTextColor"
-  },
-  {
-    colorCode: "#f2f2f2",
-    colorType: "sideBarButtonColor"
-  },
-  {
-    colorCode: "#201d1d",
-    colorType: "sideBarButtonTextColor"
-  },
-  {
-    colorCode: "#343232",
-    colorType: "buttonColor"
-  },
-  {
-    colorCode: "#fffafa",
-    colorType: "buttonTextColor"
-  },
-  {
-    colorCode: "#f2f2f2",
-    colorType: "backGroundColor"
-  },
-  {
-    colorCode: "",
-    colorType: "textColor"
-  },
-  {
-    colorCode: "#242424",
-    colorType: "titleColor"
-  },
-  {
-    colorCode: "#1a1a1a",
-    colorType: "cardColor"
-  },
-  {
-    colorCode: "#fcfcfc",
-    colorType: "cardBackGroundColor"
-  },
-  {
-    colorCode: "#fcfcfc",
-    colorType: "modelBackgroundColor"
-  },
-  {
-    colorCode: "#2b2727",
-    colorType: "modelColor"
-  }
-];
+
 
 const settingSchema = new mongoose.Schema({
   logoLight: {
@@ -96,7 +43,7 @@ const settingSchema = new mongoose.Schema({
     type: String,
     default: ''
   },
-  resetColor: {
+  defaultColor: {
     type: [
       {
         colorCode: {
@@ -109,7 +56,11 @@ const settingSchema = new mongoose.Schema({
         }
       }
     ],
-    default: defaultResetColor // Set the default value
+    default: [] // Set the default value
+  },
+  setDefault: {
+    type: Number,
+    default:0
   }
 });
 
