0b2b9adb9ee0d7f219c62739b47e8b281e68c24e
webdeveloper510 <developerweb6@gmail.com>
2024-09-09 15:42:16 +0530

    contact us key add

--- validators/User/contact_us.js
+++ validators/User/contact_us.js
@@ -3,6 +3,7 @@ const Joi = require('joi')
 const filer_contact_us = Joi.object({
     firstName:Joi.string().trim().allow(null).allow('').optional(),
     phoneNumber:Joi.string().trim().allow(null).allow('').optional(),
+    category:Joi.string().trim().allow(null).allow('').optional(),
     lastName:Joi.string().trim().allow(null).allow('').optional(),
     email:Joi.string().allow(null).allow('').optional(),
     description:Joi.string().allow(null).allow('').optional(),    
