1817396d33a49fb46b64f4dca57fb2bdefe6dfb2
webdeveloper510 <developerweb6@gmail.com>
2024-09-16 17:27:47 +0530

    commit in get cover

--- controllers/Order/orderCreateController.js
+++ controllers/Order/orderCreateController.js
@@ -102,6 +102,7 @@ exports.checkFileValidation = async (req, res) => {
             let file = req.file;
             let csvName = file.key;
             let originalName = file.originalname;
+            let message = [];
             let size = file.size;
             let totalDataComing1 = [];
             let ws;
