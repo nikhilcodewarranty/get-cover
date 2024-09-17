6687158d23a4119d587e243cb041390c0a00e1a7
webdeveloper510 <developerweb6@gmail.com>
2024-09-17 20:07:48 +0530

    dealer sku in tc

--- controllers/Order/supportingController.js
+++ controllers/Order/supportingController.js
@@ -606,7 +606,7 @@ exports.generateHtmltopdf = async (req, res) => {
                 for (let j = 0; j < checkOrder?.productsArray[i].QuantityPricing.length; j++) {
                     let quanitityProduct = checkOrder?.productsArray[i].QuantityPricing[j];
                     let obj = {
-                        productName: quanitityProduct.name,
+                        productName: checkOrder?.productsArray[i]?.dealerSku,
                         noOfProducts: quanitityProduct.enterQuantity
                     }
                     productCoveredArray.push(obj)
@@ -617,7 +617,7 @@ exports.generateHtmltopdf = async (req, res) => {
                 let findContract = contractArray.find(contract => contract.orderProductId.toString() === checkOrder?.productsArray[i]._id.toString())
 
                 let obj = {
-                    productName: findContract.productName,
+                    productName: findContract?.dealerSku,
                     noOfProducts: checkOrder?.productsArray[i].noOfProducts
                 }
                 productCoveredArray.push(obj)
