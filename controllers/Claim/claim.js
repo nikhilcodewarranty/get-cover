6862c584386e4be0df1dfde92bef31943e388cd1
webdeveloper510 <webdeveloper6@gmail.com>
2024-09-30 11:51:02 +0530

    checking the coverageType change in claim list ak

--- controllers/Claim/claim.js
+++ controllers/Claim/claim.js
@@ -739,7 +739,7 @@ exports.editClaimType = async (req, res) => {
       await LOG(logData).save()
       if(updateData.claimType!=""&&updateData.claimType!="New"){
         console.log("checking ak ++++++++++++++++++++++++++",process.env.API_ENDPOINT+"claim/checkClaimAmount/"+updateData._id)
-       let udpateclaimAmount =  await axios.get(process.env.API_ENDPOINT+"claim/checkClaimAmount/"+updateData._id, {
+       let udpateclaimAmount =  await axios.get(process.env.API_ENDPOINT+"api-v1/claim/checkClaimAmount/"+updateData._id, {
           headers: {
               "x-access-token":req.header["x-access-token"],  // Include the token in the Authorization header
           }
