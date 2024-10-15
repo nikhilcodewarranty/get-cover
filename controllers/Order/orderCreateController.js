91b1f51cffcb078d02e9ce10fbadd541b5391883
webdeveloper510 <webdeveloper510>
2024-10-15 11:04:57 +0530

    date checik

--- controllers/Order/orderCreateController.js
+++ controllers/Order/orderCreateController.js
@@ -2180,7 +2180,7 @@ exports.editOrderDetail = async (req, res) => {
                 console.log("checking the date+++++++++++++++++++++++", addOneDay2)
                 let addOneDay3 = new Date(getChoosedProducts[A].coverageStartDate)
                 console.log("checking the date+++++++++++++++++++++++", addOneDay3)
-                addOneDay2.setMonth(addOneDay2.getMonth() + getChoosedProducts[A].term)
+                addOneDay3.setMonth(addOneDay3.getMonth() + getChoosedProducts[A].term)
                 console.log("checking the date+++++++++++++++++++++++", addOneDay3)
 
                 data.productsArray[A].coverageStartDate1 = addOneDay
