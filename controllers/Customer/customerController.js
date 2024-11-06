88fdda1a287d6c634d27d542a5d52902c00e0b3e
webdeveloper510 <webdeveloper510>
2024-11-06 10:57:05 +0530

    address data ak

--- controllers/Customer/customerController.js
+++ controllers/Customer/customerController.js
@@ -2291,12 +2291,48 @@ exports.addAddress = async (req, res) => {
   }
 }
 
+exports.deleteAddress = async (req, res) => {
+  try {
+    let data = req.body
+    let checkCustomer = await customerService.getCustomerById({ _id: req.params.customerId })
+    if (!checkCustomer) {
+      res.send({
+        code: constant.errorCode,
+        message: err.message
+      })
+      return
+    }
+    let customerAddresses = checkCustomer.addresses ? checkCustomer.addresses : []
+    console.log(customerAddresses)
+    let newArray = customerAddresses.filter(obj => obj._id.toString() !== data.addressId.toString())
+    customerAddresses.push(data.address)
+    console.log("----------------------------------------------", data, customerAddresses)
+
+    let udpateCustomer = await customerService.updateCustomer({ _id: req.params.customerId }, { addresses: newArray }, { new: true })
+    if (!udpateCustomer) {
+      res.send({
+        code: constant.errorCode,
+        message: "Unable to add customer address"
+      })
+    } else {
+      res.send({
+        code: constant.successCode,
+        message: "Customer address added successfully"
+      })
+    }
+  } catch (err) {
+    res.send({
+      code: constant.errorCode,
+      message: err.message
+    })
+  }
+}
+
 exports.editaddress = async (req, res) => {
   try {
     let data = req.body
     const customerId = data.customerId;
     const addressId = data.addressId;
-
 
     let updateCustomer = await customerService.updateCustomer(
       { _id: customerId, 'addresses.addressId': addressId }, // Match the customer and specific address
