df1d2d6a5183e67097c076175fb682645c592ad6
webdeveloper510 <developerweb6@gmail.com>
2024-11-22 15:34:00 +0530

    reporting data clainm date

--- controllers/User/graphdataController.js
+++ controllers/User/graphdataController.js
@@ -383,6 +383,8 @@ exports.getDashboardGraph = async (req, res) => {
     let startOfMonth2 = new Date(new Date().setDate(new Date().getDate() - 30));
 
     let startOfMonth = new Date(startOfMonth2.getFullYear(), startOfMonth2.getMonth(), startOfMonth2.getDate());
+    
+    console.log("startOfMonth---------------",startOfMonth)
 
 
     let endOfMonth = new Date(endOfMonth1.getFullYear(), endOfMonth1.getMonth(), endOfMonth1.getDate() + 1);
