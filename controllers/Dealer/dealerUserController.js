bfc2f2e1bfc533dc521dbd0f514a39d2e6966c67
webdeveloper510 <developerweb6@gmail.com>
2024-12-07 12:33:48 +0530

    order notification changes to the api

--- controllers/Dealer/dealerUserController.js
+++ controllers/Dealer/dealerUserController.js
@@ -687,7 +687,7 @@ exports.createCustomer = async (req, res, next) => {
         res.send({
             code: constant.successCode,
             message: "Customer created successfully",
-            result: data
+            result: createdCustomer
         })
     } catch (err) {
         //Save Logs create Customer
--- controllers/Dealer/resellerUserController.js
+++ controllers/Dealer/resellerUserController.js
@@ -290,7 +290,7 @@ exports.createCustomer = async (req, res, next) => {
         res.send({
             code: constant.successCode,
             message: "Customer created successfully",
-            result: data
+            result: createdCustomer
         })
     } catch (err) {
         res.send({
