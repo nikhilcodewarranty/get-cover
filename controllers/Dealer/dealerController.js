be6e3752be4a46fb28c9695a61b81e7bae5beaef
webdeveloper510 <webdeveloper510>
2024-12-12 12:00:11 +0530

    checkning ak

--- controllers/Dealer/dealerController.js
+++ controllers/Dealer/dealerController.js
@@ -723,7 +723,7 @@ exports.createDealerPriceBook = async (req, res) => {
       return;
     }
 
-    let checkCategory = await priceBookService.getPriceCatById({ _id: new mongoose.Types.ObjectId(checkPriceBookMain.category) }, {})
+    let checkCategory = await priceBookService.getPriceCatById({ _id: new mongoose.Types.ObjectId(data.category) }, {})
 
     if (!checkCategory) {
       res.send({
