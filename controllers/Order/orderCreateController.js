5585874e92fbd3d8cef0573b28781e59ce19469e
webdeveloper510 <webdeveloper510>
2024-12-13 14:53:27 +0530

    udpate notificationd ata

--- controllers/Order/orderCreateController.js
+++ controllers/Order/orderCreateController.js
@@ -1146,8 +1146,8 @@ exports.createOrder1 = async (req, res) => {
         let count = await orderService.getOrdersCount({ 'unique_key': { '$regex': currentYear, '$options': 'i' } });
 
         data.unique_key_number = count[0] ? count[0].unique_key_number + 1 : 100000
-        data.unique_key_search = "GC" + "2024" + data.unique_key_number
-        data.unique_key = "GC-" + "2024-" + data.unique_key_number
+        data.unique_key_search = "GC" + currentYear + data.unique_key_number
+        data.unique_key = "GC-" + currentYear + "-" + data.unique_key_number
 
         let checkVenderOrder = await orderService.getOrder(
             { venderOrder: data.dealerPurchaseOrder, dealerId: data.dealerId },
@@ -1691,9 +1691,9 @@ exports.createOrder1 = async (req, res) => {
 
                     let createNotification = await userService.createNotification(notificationData1);
 
-                    console.log("term and conditon-------------------",checkOrder?.termCondition)
+                    console.log("term and conditon-------------------", checkOrder?.termCondition)
                     // Send Email code here
-                    if (!checkOrder?.termCondition || checkOrder?.termCondition==null || checkOrder?.termCondition=='') {
+                    if (!checkOrder?.termCondition || checkOrder?.termCondition == null || checkOrder?.termCondition == '') {
                         let notificationEmails = adminUsers.map(user => user.email)
                         //Email to Dealer
                         let emailData = {
