40b75f01acd0f67f264a476ea94f81ca71fec2b9
webdeveloper510 <developerweb6@gmail.com>
2024-12-31 12:46:33 +0530

    checking the data

--- controllers/User/supportingApiAdmin.js
+++ controllers/User/supportingApiAdmin.js
@@ -302,7 +302,7 @@ exports.createDealer = async (req, res) => {
                 const checkLoginUser = await supportingFunction.getPrimaryUser({ _id: req.teammateId })
 
                 let notificationData = {
-                    title: "New Dealer Approved",
+                    title: "New Dealer Added",
                     description: `A New Dealer ${data.name} has been added and approved by ${checkLoginUser.metaData[0]?.firstName + " " + checkLoginUser.metaData[0]?.lastName} on our portal.`,
                     userId: req.teammateId,
                     flag: 'dealer',
