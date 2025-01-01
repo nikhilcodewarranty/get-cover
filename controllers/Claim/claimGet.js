22f544b48538dc1357431ee447ae504249744007
webdeveloper510 <webdeveloper510>
2025-01-01 12:15:17 +0530

    checking the data

--- controllers/Claim/claimGet.js
+++ controllers/Claim/claimGet.js
@@ -298,7 +298,7 @@ exports.getAllClaims = async (req, res, next) => {
       }
     }
     let lookupQuery = [
-      { $sort: { unique_key_number: -1 } },
+      { $sort: { createdAt: -1 } },
       {
         $match:
         {
