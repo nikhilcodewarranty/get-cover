ac56a65937101192513e913acae7296fd231216a
webdeveloper510 <developerweb6@gmail.com>
2025-01-08 11:49:05 +0530

    checking the data

--- controllers/User/usersController.js
+++ controllers/User/usersController.js
@@ -3268,16 +3268,11 @@ exports.getSetting = async (req, res) => {
         }
 
         const sideBarColor = setting[0]?.colorScheme.find(color => color.colorType === "sideBarColor");
-        const chartFirstColor = setting[0]?.colorScheme.find(color => color.colorType === "chartFirstColor");
-        const exists = setting[0].colorScheme.some(color => color.colorType === 'chartFirstColor');
   
+
         if (sideBarColor) {
           setting[0].adminSideBarColor = sideBarColor;
-          setting[0].colorScheme.push({ colorType: "adminSideBarColor", colorCode: sideBarColor?.colorCode });
-        }
-        if (!exists) {
-          setting[0].adminSideBarColor = sideBarColor;
-          setting[0].colorScheme.push({ colorType: "chartFirstColor", colorCode: chartFirstColor?.colorCode });
+          setting[0].colorScheme.push({ colorType: "adminSideBarColor", colorCode: sideBarColor.colorCode });
         }
 
         // Repeat for any other properties that need the base_url prepended
