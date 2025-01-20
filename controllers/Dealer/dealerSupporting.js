c438f2a715755b5949064328b0e228033488793d
Amit8566 <aynr135001@gmail.com>
2025-01-20 17:26:10 +0530

    all servicer in the list of thr clais

--- controllers/Dealer/dealerSupporting.js
+++ controllers/Dealer/dealerSupporting.js
@@ -1615,14 +1615,13 @@ exports.getDealerAsServicerClaims = async (req, res) => {
         const dynamicOption = await userService.getOptions({ name: 'coverage_type' })
 
         //Get Dealer and Reseller Servicers
-        let servicer;
         let servicerName = '';
         allServicer = await servicerService.getAllServiceProvider(
             { _id: { $in: allServicerIds }, status: true },
             {}
         );
         let result_Array = await Promise.all(resultFiter.map(async (item1) => {
-            servicer = []
+           let servicer = []
             let mergedData = []
             if (Array.isArray(item1.contracts?.coverageType) && item1.contracts?.coverageType) {
                 mergedData = dynamicOption.value.filter(contract =>
