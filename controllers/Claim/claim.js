82ef8a7fc02c95f9c3a321a58cb7cfb8a42280a5
Amit8566 <aynr135001@gmail.com>
2025-01-23 20:05:23 +0530

    bulk changes int he dealerServicer

--- controllers/Claim/claim.js
+++ controllers/Claim/claim.js
@@ -3227,21 +3227,17 @@ exports.saveBulkClaim = async (req, res) => {
             item.exit = true;
           }
 
-          console.log("dealer++++++++++++++++++++++++++++++++", allDataArray[0]?.order.dealer)
-          console.log("servicer++++++++++++++++++++++++++++++++", allDataArray[0]?.order.dealer.dealerServicer)
           if (allDataArray.length > 0 && servicerData) {
             flag = false;
             if (allDataArray[0]?.order.dealer.dealerServicer.length > 0) {
               //Find Servicer with dealer Servicer
               const servicerCheck = allDataArray[0]?.order.dealer.dealerServicer.find(item => item.servicerId?.toString() === servicerData._id?.toString())
               if (servicerCheck) {
-                console.log("++++++++++++++++++++++++++++++++++1")
                 flag = true
               }
               //Check Dealer itself servicer
               if (allDataArray[0]?.order.dealer?.isServicer && allDataArray[0]?.order.dealer?.accountStatus && allDataArray[0]?.order.dealer._id?.toString() === servicerData.dealerId?.toString()) {
                 flag = true
-                console.log("++++++++++++++++++++++++++++++++++2")
 
               }
               //Check Dealer Reseller servicer
@@ -3261,7 +3257,6 @@ exports.saveBulkClaim = async (req, res) => {
           if ((item?.servicerName != '' && !servicerData)) {
             flag = false
           }
-          console.log("flag---------------------------",typeof(flag),flag)
           if ((!flag && flag != undefined && item.hasOwnProperty("servicerName") && req.role == "Super Admin")) {
             console.log("sdfsdfsddsffdsdfsdsfdfsdsfdfsdfsfdsdfssdf")
             item.status = "Servicer not found"
@@ -3278,8 +3273,8 @@ exports.saveBulkClaim = async (req, res) => {
         } 
       }
 
-      console.log("totalDataComing-----------------------------",totalDataComing)
-      return;
+      // console.log("totalDataComing-----------------------------",totalDataComing)
+      // return;
       // return;
       let finalArray = []
       //Save bulk claim
